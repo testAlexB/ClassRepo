33ef5fdd61cca198871866493c751f56ec817029
a.boikov <alex8oikov@yandex.ru>
2024-10-29 10:24:54 +0300

    добавили класс, отвечающий за представление информации о тоаре

---
+++ Incapsulation/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Incapsulation
+{
+    public class Product
+    {
+        private string name_;
+        private double price_;
+
+        public Product(string n, double p)
+        {
+            name_ = n;
+            price_ = p;
+        }
+
+        public double GetPrice()
+        {
+            return price_;
+        }
+    }
+}
--- Incapsulation/Program.cs
+++ Incapsulation/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Incapsulation
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            Console.ReadKey();
         }
     }
 }
