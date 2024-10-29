68a29b7a85b06f993eb7a32a4b6629b351ab450d
a.boikov <alex8oikov@yandex.ru>
2024-10-29 11:17:39 +0300

    добавили общий словарь товаров + дополнил ДЗ

--- Incapsulation/Program.cs
+++ Incapsulation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Incapsulation
 {
@@ -6,24 +7,36 @@ namespace Incapsulation
     {
         static void Main()
         {
+            Dictionary<string, Product> allProducts = FetchProductsInfo();
             SalesHistory history = new SalesHistory();
             history.AddSale(
                 new Sale(167)
-                .SetFactSale(new Product("Колбаса копченая", 1100), 0.1));
+                .SetFactSale(allProducts["Колбаса копченая"], 0.1));
 
             history.AddSale(
                 new Sale(167)
-                .SetFactSale(new Product("Творог воздушный", 550), 0.5));
+                .SetFactSale(allProducts["Творог воздушный"], 0.5));
 
             history.AddSale(
                 new Sale(7)
-                .SetFactSale(new Product("Колбаса копченая", 550), 0.7));
+                .SetFactSale(allProducts["Колбаса копченая"], 0.7));
 
             Console.WriteLine("cost = " + history.CalcCost());
 
             /// ДЗ: вывести на экран чеки (товар, дата, количество, стоимость)
+            /// + разобраться с тем, почему стало 1135. Должно быть 770
 
             Console.ReadKey();
+        }
+
+
+        private static Dictionary<string, Product> FetchProductsInfo()
+        {
+            Dictionary<string, Product> result = new Dictionary<string, Product> ();
+            result.Add("Колбаса копченая", new Product("Колбаса копченая", 1100.0));
+            result.Add("Творог воздушный", new Product("Творог воздушный", 550.0));
+
+            return result;
         }
     }
 }
