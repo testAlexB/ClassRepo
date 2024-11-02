c3cfa13655d6a60aa85c9d339ada32f5f96cb054
a.boikov <alex8oikov@yandex.ru>
2024-11-02 08:59:58 +0300

    добавили производный класс Professor, расширили характеристикой Lesson, перегрузили метод перемещения профессора в колледж

--- Inheritance/Professor.cs
+++ Inheritance/Professor.cs
@@ -1,6 +1,41 @@
-﻿namespace Inheritance
+﻿using System;
+
+namespace Inheritance
 {
-    public class Professor
+    public enum  Lesson
     {
+        Math = 0,
+        Russian,
+        English
+    }
+    public class Professor : PersonCollege
+    {
+        private Lesson lesson_;
+        public Professor(string n, int a, Lesson l) : base(n, a)
+        {
+            lesson_ = l;
+        }
+
+        public new void MovePersonToCollege()
+        {
+            Console.WriteLine("               " + name_ + ", " + LessonToString(lesson_));
+        }
+
+        public static string LessonToString(Lesson l)
+        {
+            switch (l)
+            {
+                case Lesson.Math:
+                    return "Математика";
+
+                case Lesson.Russian:
+                    return "Русский язык";
+
+                case Lesson.English:
+                    return "Английский язык";
+            }
+
+            return "Неизвестно";
+        }
     }
 }
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -9,6 +9,12 @@ namespace Inheritance
             PersonCollege person = new PersonCollege("Иван", 20);
             Console.WriteLine(person.GetName());
             person.MovePersonToCollege();
+
+
+            Professor pr = new Professor("Александр", 24, Lesson.Math);
+            Console.WriteLine(pr.GetName());
+            pr.MovePersonToCollege();
+
             Console.ReadKey();
         }
     }
