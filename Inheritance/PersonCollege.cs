e5665af976a328595c80924cf8919ce491cc0d68
a.boikov <alex8oikov@yandex.ru>
2024-11-02 08:33:07 +0300

    добавили базовый класс, описывающий информацию о любой персоне колледжа

---
+++ Inheritance/PersonCollege.cs
@@ -0,0 +1,19 @@
+﻿namespace Inheritance
+{
+    public class PersonCollege
+    {
+        protected string name_;
+        protected int age_;
+
+        public PersonCollege(string n, int a)
+        {
+            name_ = n; 
+            age_ = a;    
+        }
+
+        public string GetName()
+        { 
+            return name_;
+        }
+    }
+}
