2171d08419c1584fa2f07d12fe701f32be1803f2
a.boikov <alex8oikov@yandex.ru>
2024-11-12 11:16:58 +0300

    добавили функцию SaveToCSV, которая позволяет позволяет сохранить в файл csv исходную информацию о товарах

--- Discounts/Program.cs
+++ Discounts/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 
 namespace Discounts
 {
@@ -17,6 +19,17 @@ namespace Discounts
 
             Print(products);
 
+
+            string fileName = "products.csv";
+            if (SaveToCSV(fileName, products))
+            {
+                Console.WriteLine($"Информация в файл {Directory.GetCurrentDirectory() + "\\" + fileName} сохранена");
+            }
+            else
+            {
+                Console.WriteLine($"Сохранение в файл {Directory.GetCurrentDirectory() + "\\" + fileName} выполнить не удалось");
+            }
+
             Console.ReadKey();
         }
 
@@ -32,6 +45,31 @@ namespace Discounts
 
                 Console.WriteLine(result);
             }
+        }
+
+        static bool SaveToCSV(string fileName, List<Product> p)
+        {
+            if(File.Exists(fileName)) 
+            {
+                File.Delete(fileName);
+            }
+
+            /// создали объект типа StreamWriter, позволяющий сохранять текст в файл с названием fileName
+            StreamWriter outputFile = new StreamWriter(fileName);
+
+            foreach (Product product in p) 
+            {
+                string csvLine =  product.GetName() + ";" 
+                                  + product.GetPrice() + ";" 
+                                  + product.GetQuantity();
+
+                outputFile.WriteLine(csvLine);
+            }
+
+
+            outputFile.Dispose(); /// перенести накопленный текст в файл
+
+            return File.Exists(fileName);
         }
     }
 }
