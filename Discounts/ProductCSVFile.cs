67d51d599de50a5f30b4ae633f69c3612e261abc
a.boikov <alex8oikov@yandex.ru>
2024-11-16 09:05:49 +0300

    рефакторинг: вынесли функции сохранения и загрузки информации в файл в отдельный класс ProductCSVFile

---
+++ Discounts/ProductCSVFile.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace Discounts
+{
+    public class ProductCSVFile
+    {
+        private string fileName_;
+        public ProductCSVFile(string fileName)
+        {
+            fileName_ = fileName;
+        }
+
+        public string GetFullPathToFile()
+        {
+            return Directory.GetCurrentDirectory() + "\\" + fileName_;
+        }
+
+        public bool SaveToCSV(List<Product> p)
+        {
+            if (File.Exists(fileName_))
+            {
+                File.Delete(fileName_);
+            }
+
+            /// создали объект типа StreamWriter, позволяющий сохранять текст в файл с названием fileName_
+            StreamWriter outputFile = new StreamWriter(fileName_);
+
+            foreach (Product product in p)
+            {
+                string csvLine = product.GetName() + ";"
+                                  + product.GetPrice() + ";"
+                                  + product.GetQuantity() + ";"
+                                  + product.GetType().Name;
+
+                outputFile.WriteLine(csvLine);
+            }
+
+
+            outputFile.Dispose(); /// перенести накопленный текст в файл
+
+            return File.Exists(fileName_);
+        }
+
+        public List<Product> LoadProductsFromCSVFile()
+        {
+            List<Product> result = new List<Product>();
+
+            string contents = File.ReadAllText(GetFullPathToFile());
+            string[] rows = contents.Split('\n');
+
+            foreach (string row in rows)
+            {
+                string[] data = row.Split(';');
+                if (data.Length != 4)
+                {
+                    continue;
+                }
+
+                string type = data[3];
+                type = type.Remove(type.Length - 1);
+                Console.WriteLine(type);
+                string nameProduct = data[0];
+                double price = Convert.ToDouble(data[1]);
+                int quantity = Convert.ToInt32(data[2]);
+
+                if (type == "Milk")
+                {
+                    result.Add(new Milk(nameProduct, price, quantity));
+                }
+                else if (type == "Water")
+                {
+                    result.Add(new Water(nameProduct, price, quantity));
+                }
+                else
+                {
+                    result.Add(new Product(nameProduct, price, quantity));
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Discounts/Program.cs
+++ Discounts/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
 
 namespace Discounts
 {
@@ -19,12 +15,11 @@ namespace Discounts
             products.Add(new Water("Святой источник", 5, 10));
             products.Add(new Product("Просто товар", 1, 1));
 
-            //Print(products);
 
+            ProductCSVFile csvFile = new ProductCSVFile("products.csv");
+            string fullPathToFile = csvFile.GetFullPathToFile();
 
-            string fileName = "products.csv";
-            string fullPathToFile = Directory.GetCurrentDirectory() + "\\" + fileName;
-            if (SaveToCSV(fileName, products))
+            if (csvFile.SaveToCSV(products))
             {
                 Console.WriteLine($"Информация в файл {fullPathToFile} сохранена");
             }
@@ -33,7 +28,7 @@ namespace Discounts
                 Console.WriteLine($"Сохранение в файл {fullPathToFile} выполнить не удалось");
             }
 
-            List<Product> productsFromFile = LoadProductsFromCSVFile(fullPathToFile);
+            List<Product> productsFromFile = csvFile.LoadProductsFromCSVFile();
             Print(productsFromFile);
 
             Console.ReadKey();
@@ -51,71 +46,6 @@ namespace Discounts
 
                 Console.WriteLine(result);
             }
-        }
-
-        static bool SaveToCSV(string fileName, List<Product> p)
-        {
-            if(File.Exists(fileName)) 
-            {
-                File.Delete(fileName);
-            }
-
-            /// создали объект типа StreamWriter, позволяющий сохранять текст в файл с названием fileName
-            StreamWriter outputFile = new StreamWriter(fileName);
-
-            foreach (Product product in p) 
-            {
-                string csvLine =  product.GetName() + ";" 
-                                  + product.GetPrice() + ";" 
-                                  + product.GetQuantity() + ";"
-                                  + product.GetType().Name;
-
-                outputFile.WriteLine(csvLine);
-            }
-
-
-            outputFile.Dispose(); /// перенести накопленный текст в файл
-
-            return File.Exists(fileName);
-        }
-
-        static List<Product> LoadProductsFromCSVFile(string fileName)
-        {
-            List<Product> result = new List<Product>();
-
-            string contents = File.ReadAllText(fileName);
-            string[] rows = contents.Split('\n');
-
-            foreach (string row in rows) 
-            {
-                string[] data = row.Split(';');
-                if(data.Length != 4)
-                {
-                    continue;
-                }
-
-                string type = data[3];
-                type = type.Remove(type.Length - 1);
-                Console.WriteLine(type);
-                string nameProduct = data[0];
-                double price = Convert.ToDouble(data[1]);
-                int quantity = Convert.ToInt32(data[2]); 
-                
-                if (type == "Milk")
-                {
-                    result.Add(new Milk(nameProduct, price, quantity));
-                }
-                else if (type == "Water")
-                {
-                    result.Add(new Water(nameProduct, price, quantity));
-                }
-                else
-                {
-                    result.Add(new Product(nameProduct, price, quantity));
-                }
-            }
-
-            return result;
         }
     }
 }
