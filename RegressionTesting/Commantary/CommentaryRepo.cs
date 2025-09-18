ebd4739aea0182d7e51be9b470f952b513bc312f
a.boikov <alex8oikov@yandex.ru>
2025-09-18 12:01:56 +0300

    реализованое сохранение комментариев в рамках тестового класса CommentaryRepo, хранящие данные в памяти в структуре словаря

---
+++ RegressionTesting/Commantary/CommentaryRepo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Commantary
+{
+    public class CommentaryRepo : ICommentaryRepository
+    {
+        /// <summary>
+        ///  ключ - логин пользователя, значение - комментарии
+        /// </summary>
+        private Dictionary<string, List<string>> comments_ = new Dictionary<string, List<string>>();
+        public void SaveComment(string login, string text)
+        {
+            if (comments_.ContainsKey(login))
+            {
+                comments_[login].Add(text);
+            }
+            else
+            {
+                comments_.Add(login, new List<string>() { text});
+            }
+        }
+    }
+}
