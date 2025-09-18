e85d1b43193d158170e8309bbc6c29b5fd4459e1
a.boikov <alex8oikov@yandex.ru>
2025-09-18 12:24:15 +0300

    добавили тест и реализацию метода получения комментариев по конктретному пользователю

---
+++ RegressionTesting/Commantary/CommentaryManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Commantary
+{
+    public class CommentaryManager
+    {
+        private ICommentaryRepository repo_;
+        public CommentaryManager(ICommentaryRepository repo) 
+        {
+            repo_ = repo;
+        }
+
+        public List<string> GetUserComments(string login)
+        {
+            var data = repo_.GetAllCommentaries();
+            if(data.ContainsKey(login))
+            {
+                return data[login];
+            }
+
+            return new List<string>();
+        }
+
+        public void AddComment(string login, string comment)
+        {
+            repo_.SaveComment(login, comment);
+        }
+    }
+}
---
+++ RegressionTesting/RegressionTesting/TCommentaryManager.cs
@@ -0,0 +1,28 @@
+﻿using Commantary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace RegressionTesting
+{
+    [TestClass]
+    public class TCommentaryManager
+    {
+
+        [TestMethod]
+        public void TestGetUserComments()
+        {
+            var manager = new CommentaryManager(new CommentaryRepo());
+
+            manager.AddComment("Петр", "Я сегодня устал");
+            manager.AddComment("Петр", "Я вчера устал");
+            manager.AddComment("Петр", "Я в принципе устал");
+            manager.AddComment("Юлия", "А я никогда не устаю");
+
+            List<string> userComments = manager.GetUserComments("Петр");
+            List<string> expected = new List<string>() { "Я сегодня устал",
+                                                         "Я вчера устал",
+                                                         "Я в принципе устал"};
+            CollectionAssert.AreEqual(expected, userComments);
+        }
+    }
+}
